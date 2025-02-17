ea6ba3c548cb996b7739e9a27823c112b3c35e03
Gamabyta24 <Gamabyta@gmail.com>
2025-02-17 19:20:22 +0300

    test connect db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
+import psycopg2
+
 
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    print("Успешное подключение!", DATABASE_URL)
+except Exception as ex:
+    print("Can`t establish connection to database", ex)
+sql = "SELECT * FROM test;"
+cursor = conn.cursor()
+cursor.execute(sql)
+for row in cursor:
+    print(row)
+cursor.close()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
